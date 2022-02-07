96bc3ac4f32eefc7c136a73deb6e1466c61183f9
ronnakash <ronnakash@gmail.com>
2022-02-07 21:20:46 +0200

    user avatar with dropdown menu

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,28 +1,56 @@
-import { Navbar, Nav, NavDropdown, Button, DropdownButton, Dropdown  } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, Button, DropdownButton, Dropdown} from 'react-bootstrap'
 import logo from '../logo.svg'
 import 'bootstrap/dist/css/bootstrap.css'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AuthContext } from '../utils/authContext'
+import UserAvatar from 'react-user-avatar'
+
+
 
 const UserButton = (props: any) => {
 
-    let {user, signOut} = useContext(AuthContext);
+    const {user, signOut} = useContext(AuthContext);
 
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    }
+
+    const handleSignOut = () => {
+        signOut();
+        setShowMenu(false);
+    }
+
+
+    
     if (!user){
         return (
-            <DropdownButton className='userButton' title="Guest" id="bg-vertical-dropdown-1">
-                <Dropdown.Item eventKey='1' href='/login'>login</Dropdown.Item>
-                <Dropdown.Item eventKey='2' href='/register' >register</Dropdown.Item>
-            </DropdownButton>
+            <div>
+                <div onClick={() => {toggleMenu()}} style={{cursor : 'pointer'}}>
+                    <UserAvatar className='userButton' size="40" name="Guest" />
+                </div>
+                <Dropdown.Menu align="end" show={showMenu} className='userDrop' >
+                    <Dropdown.Item eventKey="1" href='/login' >Sign in</Dropdown.Item>
+                    <Dropdown.Item eventKey="2" href='/register' >Register</Dropdown.Item>
+                </Dropdown.Menu> 
+            </div>
+
         )
     }
     else {
-        return (            
-            <DropdownButton className='userButton' title={user.username} id="bg-vertical-dropdown-1">
-                <Dropdown.Item eventKey='1' href='/notes'>Notes</Dropdown.Item>
-                <Dropdown.Item eventKey='2' href='/profile'>Profile</Dropdown.Item>
-                <Dropdown.Item eventKey='3' onClick={signOut} className='dangerItem'>Sign out</Dropdown.Item>
-            </DropdownButton>
+        return (
+            <div>
+            <div onClick={() => {toggleMenu()}} style={{cursor : 'pointer'}}>
+                <UserAvatar className='userButton' size="40" name={user.username} src={user.picture}/>
+            </div>
+            <Dropdown.Menu align="end" show={showMenu} className='userDrop' >
+            <Dropdown.Item eventKey='3' href='/notes'>Notes</Dropdown.Item>
+                <Dropdown.Item eventKey='4' href='/profile' >Profile</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item eventKey='5' onClick={handleSignOut} className='dangerItem' >Sign out</Dropdown.Item>
+            </Dropdown.Menu> 
+        </div>            
         )
     }      
 
