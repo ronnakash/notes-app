51f468fcd1efe9363a32ebe1440900ae3613b1e5
ronnakash <ronnakash@gmail.com>
2022-09-25 15:01:31 +0300

    google sign in working

--- src/components/user/SigninForm.tsx
+++ src/components/user/SigninForm.tsx
@@ -105,7 +105,7 @@ const SigninForm = (props : {submitForm : (event : any) => void , signInWithGoog
         <Button type="submit" className='button-space'>Submit form</Button>
         <GoogleLogin
               className='googleLoginButton'
-              clientId={process.env.GOOGLE_CLIENT_ID? process.env.GOOGLE_CLIENT_ID : ""}
+              clientId={process.env.GOOGLE_CLIENT_ID? process.env.GOOGLE_CLIENT_ID : "888361755327-ad9pvtvsfpkhk09fqtpsqepbgtcapg4r.apps.googleusercontent.com"}
               onFailure={failedLogin}
               onSuccess={googleLogin}
               onRequest={loginRedirect}
